166bc48c0a0b6737d6d46c1b80110dbbaa665814
kaiser-y <kaiser.y@binance.com>
2026-02-02 17:10:39 +0800

    dev branch

--- src/main/java/org/example/codecoveragedemo/TestController.java
+++ src/main/java/org/example/codecoveragedemo/TestController.java
@@ -21,4 +21,19 @@ public class TestController {
         System.out.println("c = " + c);
         return "testJacoco";
     }
+
+    @GetMapping("/test2")
+    public String testJacoco2() {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        if (a == 0) {
+            b = 1;
+        } else {
+            b = 2;
+        }
+        c = b + 1;
+        System.out.println("c = " + c);
+        return "testJacoco";
+    }
 }
