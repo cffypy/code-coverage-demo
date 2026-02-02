40dd6ef496344feb0e955776781682fcc9fada6b
kaiser-y <kaiser.y@binance.com>
2026-02-02 17:09:50 +0800

    main branch

---
+++ src/main/java/org/example/codecoveragedemo/CodeCoverageDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.codecoveragedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCoverageDemoApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(CodeCoverageDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/example/codecoveragedemo/TestController.java
@@ -0,0 +1,24 @@
+package org.example.codecoveragedemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    @GetMapping("/test")
+    public String testJacoco() {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        if (a == 0) {
+            b = 1;
+        } else {
+            b = 2;
+        }
+        c = b + 1;
+        System.out.println("c = " + c);
+        return "testJacoco";
+    }
+}
